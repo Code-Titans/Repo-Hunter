08d0b5bda2dba82f9639f2777f13ce35ffcfb15e
Bryan-Cee <cheruiyotbryan@gmail.com>
2019-06-20 18:01:39 +0300

    Add login form and styling
    This commit add the login form and the styling according
    to the mock ups.

--- client/components/Login.js
+++ client/components/Login.js
@@ -4,16 +4,25 @@ const styles = require('../styles/Login.scss');
 const Login = (props) => {
   return (
     <div className={styles.Login}>
+      <div className={styles.LoginTriangleLeft}/>
       <form className={styles.LoginForm}>
-        <span>Sign Up</span>
+        <span>Login</span>
+        <hr/>
         <label htmlFor={"email"}>Email</label>
-        <input type="email" name={"email"}/>
+        <input type="email" name={"email"} placeholder={"janedoe@example.com"}/>
 
         <label htmlFor={"password"}>Password</label>
-        <input type="password" name={"password"}/>
-        <span><a href={"#"}>Forgot Password?</a></span>
-        <button>CREATE LOGIN</button>
+        <input type="password" name={"password"} placeholder={"*********"}/>
+        <span className={styles.LoginFormFPassword}><a href={"#"}>Forgot Password?</a></span>
+        <button type="submit">LOGIN</button>
       </form>
+      <p>or login with</p>
+      <div className={styles.LoginSocial}>
+        <a href="#"><img src={"/static/google-icon.svg"} alt="google"/></a>
+        <a href="#"><img src={"/static/github-icon.svg"} alt="github"/></a>
+      </div>
+      <p>Don't have an account? <span><a href="#">Sign up</a></span></p>
+      <div className={styles.LoginTriangleRight}/>
     </div>
   )
 };
