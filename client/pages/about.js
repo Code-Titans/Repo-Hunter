f8d121c8cdfa2a3c6b015cc63701066b54907ca0
Bryan-Cee <cheruiyotbryan@gmail.com>
2019-07-15 11:16:52 +0300

    Setup Login

--- client/pages/about.js
+++ client/pages/about.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
+import { withRouter } from 'next/router';
+import Layout from '../components/Layout';
+const styles = require('../styles/About.scss');
 
 export default class extends Component {
-  static getInitialProps ({ query: { code } }) {
-    return { aboutId: code }
-  }
-
-  render () {
-    return <div>
-      <h1>Code: {this.props.aboutId}</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </p>
-    </div>
+  render() {
+    return (
+      <Layout>
+        <div className={styles.About}>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </Layout>
+    )
   }
 }
