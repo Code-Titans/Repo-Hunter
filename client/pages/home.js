136d3656c6097529738e226f434882a5a54c7953
Bryan-Cee <cheruiyotbryan@gmail.com>
2019-07-29 15:10:50 +0300

    Complete signup and sign in
    This needs to still be worked on in a later release
    once the home page section is completed

---
+++ client/pages/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Layout from '../components/Layout';
+import { Fragment } from 'react';
+const styles = require('../styles/Home.scss');
+
+export default class extends Component {
+  render() {
+    return (
+      <Layout>
+        <Fragment>
+
+        </Fragment>
+      </Layout>
+    )
+  }
+}
