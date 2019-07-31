62e67a144db76ea4d65d94b06b315b50eb710e45
daviskimame <hoolykim@gmail.com>
2019-07-31 17:50:43 +0300

    Refactored code
    Implements the use of one method to handle the change of state for both sort by
    and filter by functionalities.

--- client/components/Dropdown.js
+++ client/components/Dropdown.js
@@ -1,35 +1,19 @@
-import React, {Component, Fragment} from 'react';
+import React, {Fragment} from 'react';
 import Select from 'react-select';
 
 const style = require('../styles/Dropdown.scss');
 
-class DropDown extends Component {
-
-  state = {
-    selectedOption: ''
-  };
-
-  handleSelectChange = (selectedOption) => {
-    this.setState({
-      selectedOption
-    }, () => console.log(this.state.selectedOption));
-
-  };
-
-  render() {
-    const {options} = this.props;
-    const {selectedOption} = this.state;
-    return (
-        <Fragment>
-          <Select
-              className={style.Select}
-              value={selectedOption}
-              onChange={this.handleSelectChange}
-              options={options}
-          />
-        </Fragment>
-    )
-  }
-}
+const DropDown = ({options, onChange, value }) => {
+  return (
+      <Fragment>
+        <Select
+            className={style.Select}
+            value={value}
+            onChange={onChange}
+            options={options}
+        />
+      </Fragment>
+  )
+};
 
 export default DropDown;
--- client/components/SortAndFilter.js
+++ client/components/SortAndFilter.js
@@ -1,29 +1,47 @@
-import React, {Fragment} from 'react';
+import React, {Component, Fragment} from 'react';
 import Select from '../components/Dropdown';
 
 const styles = require('../styles/SortAndFilter.scss');
 
 
-const options = [
-  {value: 'Most Liked', label: 'Most Liked'},
-  {value: 'Most Stared', label: 'Most Stared'},
+const filter = [
+  {value: 'Likes', label: 'Likes', name:'filter'},
+  {value: 'Stars', label: 'Stars', name:'filter'},
+];
+const sorting = [
+  {value: 'Most', label: 'Most', name:'sort'},
+  {value: 'Least', label: 'Least', name:'sort'},
 ];
 
-const SortAndFilter = () => {
-  return (
-      <Fragment>
-        <div className={styles.SearchSection}>
-          <div className={styles.SearchInput}>
-            <label> Filter by:</label>
-            <Select options={options}/>
+class SortAndFilter extends Component {
+  state = {
+    filter: '',
+    sort: ''
+  };
+
+  handleChange = ({value, label,  name}) => {
+    this.setState({
+      [name]: { value, label, name }
+    }, () => console.log(this.state.sort));
+
+  };
+
+  render() {
+    return (
+        <Fragment>
+          <div className={styles.SearchSection}>
+            <div className={styles.SearchInput}>
+              <label> Filter by:</label>
+              <Select options={filter} value={this.state.filter} onChange={this.handleChange}/>
+            </div>
+            <div className={styles.SearchInput}>
+              <label> Sort by:</label>
+              <Select options={sorting} value={this.state.sort} onChange={this.handleChange}/>
+            </div>
           </div>
-          <div className={styles.SearchInput}>
-            <label> Sort by:</label>
-            <Select options={options}/>
-          </div>
-        </div>
-      </Fragment>
-  )
-};
+        </Fragment>
+    )
+  }
+}
 
 export default SortAndFilter;
