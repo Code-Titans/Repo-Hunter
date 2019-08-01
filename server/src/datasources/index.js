3cf0b3205c827548720a0da761c578e210122394
Bryan-Cee <cheruiyotbryan@gmail.com>
2019-08-01 13:16:52 +0300

    heroku deployment
    This commit deploys the application to heroku.

--- server/src/datasources/index.js
+++
@@ -1,7 +0,0 @@
-import pgClient from './postgres';
-import MongoDbAPI from './mongodb';
-
-const mongoConfig = process.env.MONGO_URL;
-
-export const mongo = new MongoDbAPI(mongoConfig);
-export const client = pgClient;
