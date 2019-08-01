1f69d61fbfe306f1b1c6159cfe696dd242e8d1de
Bryan-Cee <cheruiyotbryan@gmail.com>
2019-08-01 13:16:52 +0300

    heroku deployment
    This commit deploys the application to heroku.

--- server/src/graphql/resolvers/user.js
+++
@@ -1,8 +0,0 @@
-const User = {
-  id: parent => parent.userId,
-  username: parent => parent.username,
-  firstName: parent => parent.firstName,
-  lastName: async parent => parent.lastName,
-};
-
-export default User;
