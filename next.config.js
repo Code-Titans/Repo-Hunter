6d7853d43087cf3d2b2378a5f09f31a4abdb9510
Bryan-Cee <cheruiyotbryan@gmail.com>
2019-08-22 15:35:45 +0300

    Fix linting errors

--- next.config.js
+++ next.config.js
@@ -3,12 +3,11 @@ const webpack = require('webpack');
 const withSass = require('@zeit/next-sass');
 
 module.exports = withSass({
-	cssModules: true,
-	webpack: (config) => {
-		config.plugins.push(
-			new webpack.EnvironmentPlugin(process.env)
-		);
-		return config
-	}
+  cssModules: true,
+  webpack: (config) => {
+    config.plugins.push(
+      new webpack.EnvironmentPlugin(process.env),
+    );
+    return config;
+  },
 });
-
