e6496d103d6ebcaf67943689a7de6290bce24f62
daviskimame <hoolykim@gmail.com>
2019-08-28 15:14:46 +0300

    Setup test environment.
    This commit setups up test environment.
    It introduces setupTest file to configure test adapter.
    It adds assetsTranformer to handle static files in test environment.
    It also adds jest configuartion file.

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  verbose: true,
+  setupFilesAfterEnv: [
+    './setupTests.js',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/assetsTransformer.js',
+    '\\.(css|scss)$': '<rootDir>/assetsTransformer.js',
+  },
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'sass',
+  ],
+  moduleDirectories: [
+    'node_modules',
+  ],
+};
